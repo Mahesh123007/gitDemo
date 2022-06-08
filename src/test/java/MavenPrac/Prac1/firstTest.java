25a9105ade93877c9409ad24834efd5d827ec62d
Naveen <singhnaveen312@gmail.com>
2022-06-08 11:11:12 +0530

    My First Commit

---
+++ src/test/java/MavenPrac/Prac1/firstTest.java
@@ -0,0 +1,17 @@
+package MavenPrac.Prac1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class firstTest {
+	@Test
+	public void testCase1() {
+		System.out.println("I am testCase1");
+	}
+
+	@Test
+	public void testCase2() {
+		System.out.println("I am testCase2");
+	}
+
+}
---
+++ src/test/java/MavenPrac/Prac1/secondTest.java
@@ -0,0 +1,17 @@
+package MavenPrac.Prac1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class secondTest {
+	@Test
+	public void testCase3() {
+		System.out.println("I am testCase3");
+	}
+
+	@Test
+	public void testCase4() {
+		System.out.println("I am testCase4");
+	}
+
+}
